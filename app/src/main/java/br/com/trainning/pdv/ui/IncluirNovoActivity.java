e65bfa9da95fd3f481cdbf4840ff7ed1a97935ef
gilmarm <gilmar.gmsousa@gmail.com>
2015-11-28 18:08:48 -0200

    implementação da Activity de incluir novos

--- app/src/main/java/br/com/trainning/pdv/ui/IncluirNovoActivity.java
+++ app/src/main/java/br/com/trainning/pdv/ui/IncluirNovoActivity.java
@@ -5,13 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.util.List;
+
 import br.com.trainning.pdv.R;
+import br.com.trainning.pdv.domain.model.Produto;
 import butterknife.Bind;
+import se.emilsjolander.sprinkles.CursorList;
+import se.emilsjolander.sprinkles.Query;
 
 public class IncluirNovoActivity extends BaseActivity {
 
@@ -49,14 +55,31 @@ public class IncluirNovoActivity extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Produto produto = new Produto();
+                produto.setId(0L);
+                produto.setDescricao(descricao.getText().toString());
+                produto.setUnidade(unidade.getText().toString());
+                produto.setCodigoBarras(codigoBarras.getText().toString());
+                produto.setPreco(Double.parseDouble(preco.getText().toString()));
+                produto.setFoto("Teste");
+                produto.setAtivo(1);
+
+                produto.save();
+
+                Log.d("INCLUIRNOVO", produto.toString());
+
+                CursorList cursor = Query.all(Produto.class).get();
+                List<Produto> lista = cursor.asList();
+
+                for(Produto p3: lista){
+                    Log.d("INCLUIRNOVO",""+p3.toString());
+                }
+                finish();
             }
         });
     }
-
 }
